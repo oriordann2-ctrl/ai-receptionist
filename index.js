d9662514fc2e041aa490f6d9dfa3313e83b9cfb7
Noel O'Riordan <oriordann2@gmail.com>
2026-04-26 15:43:06 +0100

    Add Maeve Voice

--- index.js
+++ index.js
@@ -800,11 +800,11 @@ app.post("/voice", async (req, res) => {
         },
         body: JSON.stringify({
           text,
-          model_id: "eleven_multilingual_v2",
+          model_id: "eleven_turbo_v2_5",
           voice_settings: {
-            stability: 0.45,
-            similarity_boost: 0.85,
-            style: 0.35,
+            stability: 0.65,
+            similarity_boost: 0.95,
+            style: 0.0,
             use_speaker_boost: true
           }
         })
